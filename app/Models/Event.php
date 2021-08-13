5b3a1bb5b39c65bd9067d3d1b524bc2acfe9a0b4
Deepesh Yadav <Deepesh.Yadav@concentrix.com>
2021-08-13 14:56:49 +0530

    added

---
+++ app/Models/Event.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+	 public function workshops() {
+        return $this->hasMany('App\Models\Workshop', 'event_id');
+    }
+}
